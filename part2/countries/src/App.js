e5ce3e456261426da0f368c6cb055b673b24db7a
nikmarsya <nikma944@gmail.com>
2022-08-18 19:08:45 +0800

    2.13 exercise

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,23 +1,34 @@
 import { useState,useEffect } from 'react'
 import axios from 'axios'
 
-const ShowList = ({list}) =>{
-  console.log('list length',list.length)
+
+const ShowList = ({list,handleShow,selected}) =>{
+ 
   if(list.length===1)
-    return <GetOne list={list} />
-  if(list.length<11)
-    return(list.map(country => <p key={country.name.common}>{country.name.common}</p>))
-  else
+    return <GetOne list={list[0]} />
+
+  if(list.length<11){
+    return(
+      list.map((country,id) => {
+        return(
+        <div key={country.name.common}>
+          {country.name.common} 
+          <button id={id} onClick={handleShow}>show</button>
+          {selected===id.toString()?<GetOne list={country} />: ''}
+         </div>
+    )})
+    )
+  }else
     return(<p>Too many matches specify another filter</p>)
 }
 
 const GetOne = ({list}) =>{
  
-  const name = list[0].name.common
-  const capital = list[0].capital[0]
-  const area = list[0].area
-  const flag = list[0].flag
-  const languages = Object.values(list[0].languages)
+  const name = list.name.common
+  const capital = list.capital[0]
+  const area = list.area
+  const flag = list.flag
+  const languages = Object.values(list.languages)
 
 
   return(
@@ -26,7 +37,7 @@ const GetOne = ({list}) =>{
         <p>capital {capital}</p>
         <p>area {area}</p>
         <div><strong>languages</strong><ul> {
-          languages.map(language => <li> {language} </li>)
+          languages.map(language => <li key={language}> {language} </li>)
           } </ul></div>
         <h1>{flag}</h1>
       </div>
@@ -34,30 +45,35 @@ const GetOne = ({list}) =>{
 }
 
 function App() {
+ 
   const [search,setSearch] = useState([])
   const [countries,SetCountries] = useState([])
+  const [key,setKey] =useState(' ')
+  
+  const handleShow = e =>{
+    setKey(e.target.id)
+   
+  }
 
   useEffect(()=>{
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response =>{
         SetCountries(response.data)
-        setSearch(response.data)
       })
   },[])
 
   
-  
-  
   const handleChange = e =>{
     setSearch(countries.filter(country => (country.name.common.toLowerCase()).includes(e.target.value.toLowerCase())))
-    
+    if(e.target.value==='')
+      setKey('')
   }
   return (
     <div>
       <p>Find countries: <input type='text' onChange={handleChange} /></p>
       <div>
-        <ShowList list={search} />
+        <ShowList list={search} handleShow={handleShow} selected={key} />
       </div>     
     </div>
   )
