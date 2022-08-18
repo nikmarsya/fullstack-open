c4a212dc550e85333218d568c67595cb2b605f6c
nikmarsya <nikma944@gmail.com>
2022-08-18 12:42:24 +0800

    exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
+const ShowList = ({list}) =>{
+  console.log('list length',list.length)
+  if(list.length===1)
+    return <GetOne list={list} />
+  if(list.length<11)
+    return(list.map(country => <p key={country.name.common}>{country.name.common}</p>))
+  else
+    return(<p>Too many matches specify another filter</p>)
+}
+
+const GetOne = ({list}) =>{
+ 
+  const name = list[0].name.common
+  const capital = list[0].capital[0]
+  const area = list[0].area
+  const flag = list[0].flag
+  const languages = Object.values(list[0].languages)
+
+
+  return(
+      <div>
+        <h1>{name}</h1>
+        <p>capital {capital}</p>
+        <p>area {area}</p>
+        <div><strong>languages</strong><ul> {
+          languages.map(language => <li> {language} </li>)
+          } </ul></div>
+        <h1>{flag}</h1>
+      </div>
+  )
+}
 
 function App() {
+  const [search,setSearch] = useState([])
+  const [countries,SetCountries] = useState([])
+
+  useEffect(()=>{
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response =>{
+        SetCountries(response.data)
+        setSearch(response.data)
+      })
+  },[])
+
+  
+  
+  
+  const handleChange = e =>{
+    setSearch(countries.filter(country => (country.name.common.toLowerCase()).includes(e.target.value.toLowerCase())))
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Find countries: <input type='text' onChange={handleChange} /></p>
+      <div>
+        <ShowList list={search} />
+      </div>     
     </div>
-  );
+  )
 }
+
+
 
 export default App;
