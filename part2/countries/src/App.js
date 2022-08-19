1eb46cc425b1f55e40a2e2c4926b527d34d56ada
nikmarsya <nikma944@gmail.com>
2022-08-19 15:49:37 +0800

    exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,9 +4,14 @@ import axios from 'axios'
 
 const ShowList = ({list,handleShow,selected}) =>{
  
-  if(list.length===1)
-    return <GetOne list={list[0]} />
-
+  if(list.length===1){
+        return (
+          <div>
+        <GetOne list={list[0]} />
+        <GetWeather list = {list[0]} />
+        </div>
+        )
+  }
   if(list.length<11){
     return(
       list.map((country,id) => {
@@ -22,15 +27,47 @@ const ShowList = ({list,handleShow,selected}) =>{
     return(<p>Too many matches specify another filter</p>)
 }
 
-const GetOne = ({list}) =>{
- 
+const GetWeather = ({list}) =>{
+  const capital = list.capital[0]
+  const lat =list.latlng[0] 
+  const lon =list.latlng[1]  
+  const w_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`
+  const [wInfo,setWInfo] = useState({"temp":0,"wind":0,"icon":"10d"})
+  
+
+    useEffect(() => {
+      axios
+      .get(w_url)
+      .then(res =>{
+       const obj = {
+        "temp":res.data.main.temp,
+        "wind":res.data.wind.speed,
+        "icon":res.data.weather[0].icon
+       } 
+       setWInfo(obj)
+        
+      })
+       // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]); 
+
+  return(
+
+        <div>
+          <h3>Weather in {capital}</h3>
+          <p>temperature {wInfo.temp} Celcius</p>
+          <img src={`http://openweathermap.org/img/wn/${wInfo.icon}.png`} alt='weather'/>
+          <p>wind {wInfo.wind} m/s</p>
+        </div>
+      )
+
+}
+const GetOne =  ({list}) =>{    
   const name = list.name.common
   const capital = list.capital[0]
   const area = list.area
   const flag = list.flag
   const languages = Object.values(list.languages)
-
-
+ 
   return(
       <div>
         <h1>{name}</h1>
@@ -49,7 +86,8 @@ function App() {
   const [search,setSearch] = useState([])
   const [countries,SetCountries] = useState([])
   const [key,setKey] =useState(' ')
-  
+
+
   const handleShow = e =>{
     setKey(e.target.id)
    
@@ -61,6 +99,7 @@ function App() {
       .then(response =>{
         SetCountries(response.data)
       })
+         
   },[])
 
   
