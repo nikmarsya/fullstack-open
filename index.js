2da64d021745085173fd454d5f00485e635463db
nikmarsya <nikma944@gmail.com>
2022-08-21 21:18:01 +0800

    uibuild

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 
 morgan.token('data',(req, res)=>{ return JSON.stringify(req.body) })
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
